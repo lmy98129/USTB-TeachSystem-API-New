2c2742b290474bd9bbf1061fb7cf2a9de6abfe26
lmy98129 <41624140@xs.ustb.edu.cn>
2018-11-25 02:54:01 +0800

    changed the hostname

--- app/router.js
+++ app/router.js
@@ -5,9 +5,8 @@
  */
 module.exports = app => {
   const { router, controller } = app;
-  // router.get('/', controller.home.index);
   router.post('/table', controller.table.index);
-  router.get('/app', controller.course.index);
+  router.get('/', controller.course.index);
   router.post('/today', controller.today.index);
   router.get('/login', controller.login.handshake);
   router.post('/login', controller.login.index);
